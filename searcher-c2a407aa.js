498e6e716e48f8678f25c3f4b5b6c2c675147a9e
Andy Aragon <hi@andymai.com>
2026-04-21 21:02:04 +0000

    deploy: 9a4c2e0b239a56253b51fb520093609799437771

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-23079b6b.js',
+                path_to_root + 'searchindex-c0dc3e4f.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
