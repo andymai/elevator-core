c9469820514dc8c89b06f0a6e3f095d49b2e820f
andymai <andymai@users.noreply.github.com>
2026-04-30 22:31:56 +0000

    deploy: 2f2a3f7a163c59abf05f2344166e6e3473f9c5f0

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-1d2481f9.js',
+                path_to_root + 'searchindex-ce8235a6.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
